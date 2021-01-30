65f0efdff0b8d2aa1a6e365aeccb7a40564e020c
Priit Matttus <priit.mattus@tptlive.ee>
2021-01-30 13:51:28 +0200

    added calculator layout

---
+++ FirstApp/FirstApp/CalculatorActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstApp
+{
+    [Activity(Label = "CalculatorActivity")]
+    public class CalculatorActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.calculator_layout);
+        }
+    }
+}
--- FirstApp/FirstApp/MainActivity.cs
+++ FirstApp/FirstApp/MainActivity.cs
@@ -15,7 +15,22 @@ namespace FirstApp
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            var calculatorButton = FindViewById<Button>(Resource.Id.calculatorButton);
+
+            button.Click += delegate
+            {
+                textView.Text = "Hello World!";
+            };
+
+            calculatorButton.Click += delegate
+            {
+                
+            };
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
