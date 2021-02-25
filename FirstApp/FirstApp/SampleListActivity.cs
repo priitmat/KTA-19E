c3a76ec6846390adb550a9d12051475dcdee8226
Priit Matttus <priit.mattus@tptlive.ee>
2021-02-25 18:11:52 +0200

    Added test cars

--- FirstApp/FirstApp/SampleListActivity.cs
+++ FirstApp/FirstApp/SampleListActivity.cs
@@ -33,6 +33,15 @@ namespace FirstApp
                 new Car { Manufacturer = "Ford5", Model = "Focus5", KW = 1050 },
                 new Car { Manufacturer = "Ford6", Model = "Focus6", KW = 74 },
                 new Car { Manufacturer = "Ford7", Model = "Focus7", KW = 5 },
+                new Car { Manufacturer = "Ford8", Model = "Focus8", KW = 86 },
+                new Car { Manufacturer = "Ford", Model = "Focus", KW = 100 },
+                new Car { Manufacturer = "Volkswagen", Model = "Passat", KW = 45 },
+                new Car { Manufacturer = "Ford2", Model = "Focus2", KW = 45 },
+                new Car { Manufacturer = "Ford3", Model = "Focus3", KW = 67 },
+                new Car { Manufacturer = "Ford4", Model = "Focus4", KW = 6 },
+                new Car { Manufacturer = "Ford5", Model = "Focus5", KW = 1050 },
+                new Car { Manufacturer = "Ford6", Model = "Focus6", KW = 74 },
+                new Car { Manufacturer = "Ford7", Model = "Focus7", KW = 5 },
                 new Car { Manufacturer = "Ford8", Model = "Focus8", KW = 86 }
             };
 
