88a911cbea3447289a87ff4af09f3fd2d8cf1316
Priit Matttus <priit.mattus@tptlive.ee>
2021-02-25 18:32:54 +0200

    custom car row layout

--- FirstApp/FirstApp/CarAdapter.cs
+++ FirstApp/FirstApp/CarAdapter.cs
@@ -42,9 +42,10 @@ namespace FirstApp
         {
             View view = convertView;
             if (view == null)
-                view = _context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem2, null);
-            view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = _items[position].Manufacturer;
-            view.FindViewById<TextView>(Android.Resource.Id.Text2).Text = _items[position].Model;
+                view = _context.LayoutInflater.Inflate(Resource.Layout.car_row_layout, null);
+            view.FindViewById<TextView>(Resource.Id.manufacturerTextView).Text = _items[position].Manufacturer;
+            view.FindViewById<TextView>(Resource.Id.modelTextView).Text = _items[position].Model;
+            view.FindViewById<TextView>(Resource.Id.kwTextView).Text = _items[position].KW.ToString();
             return view;
         }
     }
