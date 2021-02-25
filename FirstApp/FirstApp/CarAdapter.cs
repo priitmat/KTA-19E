cbf4d29fbc6be945457b9696aa8936f9bd5c7172
Priit Matttus <priit.mattus@tptlive.ee>
2021-02-25 18:02:02 +0200

    Added CarAdapter

---
+++ FirstApp/FirstApp/CarAdapter.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FirstApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstApp
+{
+    public class CarAdapter : BaseAdapter<Car>
+    {
+        List<Car> _items;
+        Activity _context;
+
+        public CarAdapter(Activity context, List<Car> items)
+        {
+            _context = context;
+            _items = items;
+        }
+
+        public override Car this[int position]
+        {
+            get { return _items[position]; }
+        }
+
+        public override int Count
+        {
+            get { return _items.Count; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            if (view == null)
+                view = _context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem2, null);
+            view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = _items[position].Manufacturer;
+            view.FindViewById<TextView>(Android.Resource.Id.Text2).Text = _items[position].Model;
+            return view;
+        }
+    }
+}
--- FirstApp/FirstApp/SampleListActivity.cs
+++ FirstApp/FirstApp/SampleListActivity.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using FirstApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace FirstApp
     [Activity(Label = "SampleListActivity")]
     public class SampleListActivity : Activity
     {
-        string[] items;
+        List<Car> items;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -22,8 +23,20 @@ namespace FirstApp
 
             var listView = FindViewById<ListView>(Resource.Id.listView1);
 
-            items = new string[] { "Volkswagen", "BMW", "Fiat" };
-            listView.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, items);
+            items = new List<Car> 
+            { 
+                new Car { Manufacturer = "Ford", Model = "Focus", KW = 100 },
+                new Car { Manufacturer = "Volkswagen", Model = "Passat", KW = 45 },
+                new Car { Manufacturer = "Ford2", Model = "Focus2", KW = 45 },
+                new Car { Manufacturer = "Ford3", Model = "Focus3", KW = 67 },
+                new Car { Manufacturer = "Ford4", Model = "Focus4", KW = 6 },
+                new Car { Manufacturer = "Ford5", Model = "Focus5", KW = 1050 },
+                new Car { Manufacturer = "Ford6", Model = "Focus6", KW = 74 },
+                new Car { Manufacturer = "Ford7", Model = "Focus7", KW = 5 },
+                new Car { Manufacturer = "Ford8", Model = "Focus8", KW = 86 }
+            };
+
+            listView.Adapter = new CarAdapter(this, items);
 
 
             // Create your application here
